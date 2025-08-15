3318b90f2fdc8f49f50ddee8941747a179874802
Amah Gordon <amah.gordon@gmail.com>
2025-08-15 15:23:52 +0000

    Corrected folder path for bookshelf app

---
+++ advanced_features_and_security/bookshelf/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.conf import settings # C'est cette ligne que vous devez ajouter
+
+# Custom User Manager
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError('The Username field must be set')
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        
+        return self.create_user(username, password, **extra_fields)
+
+# Custom User Model
+class CustomUser(AbstractUser):
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_groups',
+        blank=True,
+        help_text='The groups this user belongs to.'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions',
+        blank=True,
+        help_text='Specific permissions for this user.'
+    )
+
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+    
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+
+class Book(models.Model):
+    # La ligne suivante crée la relation avec l'utilisateur
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
