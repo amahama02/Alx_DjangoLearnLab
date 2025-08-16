30b6e2f9fd2e1622a8b56a4b357bd0cdc8b24165
Amah Gordon <amah.gordon@gmail.com>
2025-08-16 17:41:55 +0000

    feat: Completed custom user model and resolved all errors

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from .models import Book
 
-# Create your views here.
+class BookListView(PermissionRequiredMixin, ListView):
+    model = Book
+    template_name = 'bookshelf/book_list.html'
+    context_object_name = 'books'
+    permission_required = 'bookshelf.can_view'
+    raise_exception = True
+
+    def get_queryset(self):
+        return Book.objects.all()
